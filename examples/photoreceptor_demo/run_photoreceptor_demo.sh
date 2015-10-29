548e98572eda2413b0406ed59e7166bdd6894a19
kpsychas <kpsychas@huxley.ee.columbia.edu>
2015-10-29 15:51:47 -0400

    added scripts

---
+++ examples/photoreceptor_demo/run_photoreceptor_demo.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-c CONF_FILE] [-r REPEAT] [-h]
+    -c  configuration file
+    -r  repeat script execution this number of times
+    -h  prints this help message
+" 1>&2;}
+
+conf=photoreceptor_default.cfg
+image=image1.mat
+rep=1
+
+while getopts "hc:r:" arg; do
+    case "$arg" in
+        c)
+            conf=${OPTARG}
+            ;;
+        r)
+            rep=${OPTARG}
+            ;;
+        h)
+            usage
+            exit
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+# add extension if not exists
+if [[ $conf != *.cfg ]]; then
+    file=$conf.cfg
+fi
+
+# create default(empty) configuration if file not exists
+if [ ! -f $conf ]; then
+    echo 'Configuration file' $conf 'does not exist. Generating ' \
+        'default configuration.'
+    touch $conf
+fi
+
+# if image does not exist in current folder make a copy
+if [ ! -f $image ]; then
+    echo 'Making a local copy of ' $image
+    cp ../$image $image
+fi
+
+echo 'Using' $conf 'configuration file, executing' $rep 'times'
+if (( $rep > 1 )) ; then
+  for (( i=0; i<$rep; i++ ))
+  do
+    python photoreceptor_demo.py -c $conf -v $i
+  done
+else
+    python photoreceptor_demo.py -c $conf -v -1
+fi
