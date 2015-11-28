21f68ac2b86d0ddc119676b258f1a1329a4090b0
kpsychas <kpsychas@huxley.ee.columbia.edu>
2015-11-28 03:02:16 -0500

    removed unused code from photoreceptor input generation file

--- examples/photoreceptor_demo/gen_input.py
+++ examples/photoreceptor_demo/gen_input.py
@@ -1,22 +1,9 @@
 from __future__ import division
 
-import argparse
-import atexit
-
-import numpy as np
-import pycuda.driver as cuda
-
 import neurokernel.LPU.utils.simpleio as sio
 from neurokernel.tools.timing import Timer
 
-import retina.retina as ret
-import retina.geometry.hexagon as hx
-import retina.classmapper as cls_map
-
 from retina.input.photoreceptor_input import get_singleinput_cls
-
-DEMO_1 = 'master'
-DEMO_2 = 'distributed'
 
 
 def gen_input_norfscreen(config):
@@ -35,23 +22,4 @@ def gen_input_norfscreen(config):
         config['General']['steps'] = photor_inputs[:, 0].size
 
     sio.write_array(photor_inputs, filename=input_file)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--rings', type=int,
-                        default=14,
-                        help='number of layers of ommatidia on circle')
-    parser.add_argument('-l', dest='sublpus', type=int,
-                        default=0,
-                        help='number of sublpus')
-    parser.add_argument('--steps', default=1000, type=int,
-                        help='simulation steps')
-    args = parser.parse_args()
-
-    gen_input(args.steps, args.rings, 1, args.sublpus)
-
-
-if __name__ == '__main__':
-    main()
 
