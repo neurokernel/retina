f4284b60351b4554ab6788a1ad7fa2e89c12b39b
Yiyin Zhou <yiyin@ee.columbia.edu>
2016-01-05 11:08:18 -0500

    updated setup.py script

--- setup.py
+++ setup.py
@@ -1,14 +1,65 @@
 #!/usr/bin/env python
 
-import setuptools
-from distutils.core import setup
+import sys, os
 
-setup(name='neurokernel-retina',
-      version='1.0',
-      packages=['retina', 'retina.vision_models',
-                'retina.neurons', 'retina.synapses', 'retina.geometry'],
-      install_requires=[
-        'configobj >= 5.0.0',
-        'neurokernel >= 0.1'
-      ]
+# Install setuptools if it isn't available:
+try:
+    import setuptools
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+NAME =               'neurokernel-retina'
+VERSION =            '1.0'
+AUTHOR =             'Neurokernel Development Team'
+AUTHOR_EMAIL =       'neurokernel-dev@columbia.edu'
+URL =                'https://github.com/neurokernel/retina'
+MAINTAINER =         AUTHOR
+MAINTAINER_EMAIL =   AUTHOR_EMAIL
+DESCRIPTION =        'Retina Model for Neurokernel'
+LONG_DESCRIPTION =   DESCRIPTION
+DOWNLOAD_URL =       URL
+LICENSE =            'BSD'
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development']
+
+# Explicitly switch to parent directory of setup.py in case it
+# is run from elsewhere:
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+PACKAGES =           find_packages()
+
+if __name__ == "__main__":
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = AUTHOR,
+        author_email = AUTHOR_EMAIL,
+        license = LICENSE,
+        classifiers = CLASSIFIERS,
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        url = URL,
+        maintainer = MAINTAINER,
+        maintainer_email = MAINTAINER_EMAIL,
+        packages = PACKAGES,
+        include_package_data = True,
+        install_requires=[
+            'configobj >= 5.0.0',
+            'neurokernel >= 0.1',
+            'scikit-cuda >= 0.5.0'
+        ]
      )
